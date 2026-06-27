0ca12f7a2ebd820b2058932591ba4c69b16888a2
Vampire <w807098553@outlook.com>
2026-06-28 01:39:58 +0800

    1

--- DXMainClient/CopyDlls.bat
+++ DXMainClient/CopyDlls.bat
@@ -2,7 +2,7 @@ rem 通过注册表获取桌面路径
 set desk_rq=REG QUERY "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "Desktop"
 for /F "tokens=3,*" %%d in ('%desk_rq%') do set desk_path=%%d
 
-set dtaDllDir=%desk_path%\AGWar1.2LT\Resources\Binaries\Windows
+set dtaDllDir=%desk_path%\AGWarDTA\Resources\Binaries\Windows
 set TargetPath=%1
 set TargetDir=%2
 
@@ -11,7 +11,8 @@ echo TargetDir:%TargetDir%
 echo SrcPath:%TargetPath%
 echo CopyDir:%dtaDllDir%
 
-if exist %dtaDllDir% xcopy /y /e /d %TargetPath% %dtaDllDir%
-if exist %dtaDllDir% xcopy /y /e /d %TargetDir%\ClientCore.dll %dtaDllDir%
-if exist %dtaDllDir% xcopy /y /e /d %TargetDir%\ClientGUI.dll %dtaDllDir%
-if exist %dtaDllDir% xcopy /y /e /d %TargetDir%\DTAConfig.dll %dtaDllDir%
+if exist %dtaDllDir% xcopy /y /e /d /f %TargetPath% %dtaDllDir%
+if exist %dtaDllDir% xcopy /y /e /d /f %TargetDir%\ClientCore.dll %dtaDllDir%
+if exist %dtaDllDir% xcopy /y /e /d /f %TargetDir%\ClientGUI.dll %dtaDllDir%
+if exist %dtaDllDir% xcopy /y /e /d /f %TargetDir%\DTAConfig.dll %dtaDllDir%
+if exist %dtaDllDir% xcopy /y /e /d /f %TargetDir%\DTAClient.Online.Core.dll %dtaDllDir%
